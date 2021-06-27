373822768d947f3f78f808f33d69132162f94c32
Jordan <jordanlevy96@gmail.com>
2021-06-27 20:45:00 +0100

    cache monster data in local json files

--- node/open5e.js
+++ node/open5e.js
@@ -1,40 +1,54 @@
-const request = require('superagent');
+const request = require("superagent");
+const fs = require("fs-extra");
 
-const { timedFunction, sizeOf } = require('./utils');
+const { timedFunction, sizeOf, writeToFile } = require("./utils");
 
-const BaseURL = 'https://api.open5e.com';
+const BaseURL = "https://api.open5e.com";
 const MonsterAPI = `${BaseURL}/monsters/`;
 
 const getAllMonsters = async () => {
-    const monsters = {};
+  let monsters = {};
+
+  if (!(await fs.pathExists("data/monsters.json"))) {
     let url = MonsterAPI;
 
     while (url) {
-        const response = await request.get(url);
-        response.body.results.map((result) => {
-            monsters[result.name] = result;
-        });
-        url = response.body.next;
+      const response = await request.get(url);
+      response.body.results.map((result) => {
+        monsters[result.name] = result;
+      });
+      url = response.body.next;
     }
+    console.log(`Monster Size: ${sizeOf(monsters)}`);
+    await fs.writeJSON("data/monsters.json", monsters);
+    // if monsters data hasn't been written, gather other monster data
+    await writeMonstersBySource(monsters);
+  } else {
+    monsters = await fs.readJSON("data/monsters.json");
+  }
 
-    console.log(`Monster Size: ${sizeOf(monsters)}`)
+  return monsters;
+};
 
-    return monsters;
-}
-
-const test = async () => {
-    try {
-        const data = await timedFunction(getAllMonsters);
-
-        return data;
+const writeMonstersBySource = async (data) => {
+  try {
+    const names = Object.keys(data);
+    const monstersBySource = {};
+    for (n of names) {
+      const monster = data[n];
+      const source = monster.document__title;
+      if (!monstersBySource[source]) {
+        monstersBySource[source] = [];
+      }
+      monstersBySource[source].push(n);
     }
-    catch (error) {
-        console.log(`Failed Open5e Test: ${error.stack}`);
-        throw error;
-    }
+    await fs.writeJSON("data/monstersBySource.json", monstersBySource);
+  } catch (error) {
+    console.log(`Failed Open5e Test: ${error.stack}`);
+    throw error;
+  }
 };
 
 module.exports = {
-    test,
-    getAllMonsters
-}
+  getAllMonsters
+};
